adb24b243205bc6198f1490526abee3d306a395a
yotaiyo <yota071218@gmail.com>
2020-07-31 16:21:59 +0900

    create companyPageLayout resolver

--- lib/resolvers.ts
+++ lib/resolvers.ts
@@ -1,9 +1,61 @@
-import { QueryResolvers } from './type-defs.graphqls'
+import { QueryResolvers, CompanyPageLayout } from './type-defs.graphqls'
 import { ResolverContext } from './apollo'
+
+const companyPageLayout_1: CompanyPageLayout = {
+  id: '1',
+  infos: [
+    {
+      __typename: 'Product',
+      id: '1',
+      title: 'google chrome',
+      url: 'https://www.google.com/intl/ja_jp/chrome/'
+    },
+    {
+      __typename: 'Officer',
+      id: '2',
+      firstName: 'Yota',
+      lastName: 'Anashige'
+    },
+    {
+      __typename: 'QuestionAndAnswer',
+      id: '3',
+      question: 'あなたの会社の強みは？',
+      answer: '粘り強いところです'
+    },
+  ]
+}
+
+const companyPageLayout_2: CompanyPageLayout = {
+  id: '2',
+  infos: [
+    {
+      __typename: 'QuestionAndAnswer',
+      id: '3',
+      question: 'あなたの会社の強みは？',
+      answer: '粘り強いところです'
+    },
+    {
+      __typename: 'Officer',
+      id: '2',
+      firstName: 'Yota',
+      lastName: 'Anashige'
+    },
+    {
+      __typename: 'Product',
+      id: '1',
+      title: 'google chrome',
+      url: 'https://www.google.com/intl/ja_jp/chrome/'
+    }
+  ]
+}
 
 const Query: Required<QueryResolvers<ResolverContext>> = {
   viewer(_parent, _args, _context, _info) {
     return { id: String(1), name: 'John Smith', status: 'cached' }
+  },
+  companyPageLayout(_parent, _args, _context, _info) {
+    const target = [companyPageLayout_1, companyPageLayout_2].find(e => e.id === _args.id)
+    return target ? target : null
   },
 }
 
