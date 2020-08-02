7987b8c3696c1f68765251a87342dbcc48aa6574
yotaiyo <yota071218@gmail.com>
2020-08-02 23:04:21 +0900

    modified indexPage

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,48 @@ import { useCompanyPageLayoutQuery, CompanyPageLayoutDocument } from '../lib/com
 import { initializeApollo } from '../lib/apollo'
 
 const Index = () => {
-  const { data } = useCompanyPageLayoutQuery()
-  console.log(data)
+  const { data, loading } = useCompanyPageLayoutQuery()
+  const companyPageLayout = data ? data.companyPageLayout : null
+  const companyInfos = companyPageLayout ? companyPageLayout.companyInfos : []
+
+  if (loading) {
+    return <div>loading</div>
+  }
 
   return (
     <div>
-      test
+      {companyInfos.map(e => {
+        if (e.__typename === 'Product') {
+          return (
+            <div key={e.id}>
+              <div>This is Product!</div>
+              <ul>
+                <li>{e.title}</li>
+                <li>{e.url}</li>
+              </ul>
+            </div>)
+        }
+        if (e.__typename === 'Officer') {
+          return (
+            <div key={e.id}>
+              <div>This is Officer!</div>
+              <ul>
+                <li>{e.firstName}</li>
+                <li>{e.lastName}</li>
+              </ul>
+            </div>)
+        }
+        if (e.__typename === 'QuestionAndAnswer') {
+          return (
+            <div key={e.id}>
+              <div>This is QuestionAndAnswer!</div>
+              <ul>
+                <li>{e.question}</li>
+                <li>{e.answer}</li>
+              </ul>
+            </div>)
+        }
+      })}
     </div>
   )
 }
